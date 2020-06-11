888a4a6858e63594750c5541196c57112b26c18c
Rashid Razak <bib.nexus@gmail.com>
2020-06-12 03:40:51 +0800

    Add onSubmit callback handler for the form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,10 +9,16 @@ class SearchBar extends React.Component {
     })
   }
 
+  onFormSubmit(event) {
+    event.preventDefault()
+
+    // TODO: Make sure to call callback from parent component
+  }
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit.bind(this)}>
           <div className="field">
             <label>Video Search</label>
             <input type="text" value={this.state.term} onChange={this.onInputChange.bind(this)} />
